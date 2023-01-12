80af3c347fdd7b89440d560e553cac0e0b82f6bd
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2023-01-12 17:06:17 +0100

    Creating components and screens

--- messengerApp/App.js
+++ messengerApp/App.js
@@ -13,14 +13,40 @@ import {
 
 
 
-const App = () => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Header from './src/components/Header';
+import ChatsScreen from './src/screens/BottomTabNavigatorScreens/ChatsScreen';
+import CallsScreen from './src/screens/BottomTabNavigatorScreens/CallsScreen';
+import SettingsScreen from './src/screens/BottomTabNavigatorScreens/SettingsScreen';
+
+const Stack = createNativeStackNavigator()
+const BottomTabs = createBottomTabNavigator()
 
 
+const ScreensTabs = () => {
   return (
-    <SafeAreaView>
+    <BottomTabs.Navigator screenOptions={{
 
+    }}>
+      <BottomTabs.Screen name='ChatsScreen' component={ChatsScreen} />
+      <BottomTabs.Screen name='Calls Screen' component={CallsScreen} />
+      <BottomTabs.Screen name='Settings' component={SettingsScreen} />
+    </BottomTabs.Navigator>
+  )
+}
 
-    </SafeAreaView>
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScreensTabs" component={ScreensTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Header" component={Header} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ messengerApp/src/const/styles.js
@@ -0,0 +1,18 @@
+export const GlobalStyles = {
+    colors: {
+        greenOne: '#b9fbc0',
+        greenTwo: '#06d6a0',
+        greenThree: '#80ffdb',
+        greenFour: '#93e1d8',
+        blueOne: '#42a5f5',
+        blueTwo: '#00bbf9',
+        blueThree: '#9bf6ff',
+        black: '#0b090a',
+        white: '#ffffff',
+        greyOne: '#dcdcdd',
+        greyThree: '#e6e8e6'
+
+
+
+    }
+}
