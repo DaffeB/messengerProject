57e068c6bcc75aa37b56918d799d7d1afb420704
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2023-01-23 16:50:14 +0100

    /

---
+++ messengerApp/src/components/Cusomisation.js
@@ -0,0 +1,86 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Image } from 'react-native'
+import { GlobalStyles } from '../const/styles'
+
+const Cusomisation = () => {
+    return (
+        <View style={{ paddingHorizontal: 16, width: '100%' }} >
+            <Text style={styles.title}>Cusomisation</Text>
+
+            <View style={{ borderWidth: 1, paddingLeft: 16, borderRadius: 10, height: '66%', borderColor: GlobalStyles.colors.greyFive }}>
+                <View style={[styles.details, { marginTop: 10 }]}>
+                    <Image style={styles.icons} source={require('../assets/iconsFriendsProfile/themeIcon.png')} />
+
+
+                    <View style={styles.bottomLine}>
+                        <Text style={styles.detailsInfo}>
+                            Theme</Text>
+
+                    </View>
+
+                </View>
+
+                <View style={styles.details}>
+                    <Image style={styles.icons} source={require('../assets/iconsFriendsProfile/lips.png')} />
+                    <View style={styles.bottomLine}>
+                        <Text style={styles.detailsInfo}>Quick reaction</Text>
+                    </View>
+                </View>
+
+                <View style={styles.details}>
+                    <Image style={styles.icons} source={require('../assets/iconsFriendsProfile/nicknamesIcon.png')} />
+                    <View style={styles.bottomLine}>
+                        <Text style={styles.detailsInfo}>Nicknames</Text>
+                    </View>
+                </View>
+
+                <View style={[styles.details, { marginBottom: 10 }]}>
+                    <Image style={styles.icons} source={require('../assets/iconsFriendsProfile/wordsEffectIcon.png')} />
+                    <View>
+                        <Text style={styles.detailsInfo}>Words effect</Text>
+                    </View>
+
+                </View>
+            </View>
+        </View >
+    )
+}
+
+export default Cusomisation
+
+const styles = StyleSheet.create({
+    title: {
+        paddingBottom: 10,
+        color: GlobalStyles.colors.greyFive
+
+    },
+
+    icons: {
+        width: 20,
+        height: 20,
+        marginRight: 20
+    },
+    details: {
+
+        flexDirection: 'row',
+        height: '26%',
+        paddingVertical: 15,
+        paddingLeft: 10,
+
+
+    },
+    bottomLine: {
+        borderBottomColor: GlobalStyles.colors.greyFive,
+        borderBottomWidth: 1,
+        width: '88%',
+        paddingVertical: 0
+
+
+    },
+    detailsInfo: {
+        fontSize: 15,
+        fontWeight: '600'
+    }
+
+})
--- messengerApp/src/screens/ProfileScreenDetails.js
+++ messengerApp/src/screens/ProfileScreenDetails.js
@@ -1,16 +1,49 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Image } from 'react-native'
+import Cusomisation from '../components/Cusomisation'
 
 const ProfileScreenDetails = () => {
 
 
     return (
         <View>
-            <Text>ProfileScreenDetails</Text>
+
+            <View>
+                <View style={styles.content}>
+                    <Image style={{ width: 70, height: 70 }} source={require('../assets/images/users/man1.png')} />
+                    <Text style={styles.friendProfileName}>Aley</Text>
+                    <View style={{ flexDirection: 'row', paddingTop: 20, paddingBottom: 20, width: 100, justifyContent: 'space-around' }}>
+                        <Image source={require('../assets/iconsFriendsProfile/facebookIcon.png')} />
+                        <Image source={require('../assets/iconsFriendsProfile/bellIcon.png')} />
+                    </View>
+
+
+                </View>
+                <Cusomisation />
+
+
+                <View>
+
+                </View>
+
+
+            </View>
         </View>
     )
 }
 
 export default ProfileScreenDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    content: {
+        alignItems: 'center',
+        marginTop: 20
+    },
+    friendProfileName: {
+        fontWeight: '800',
+        fontSize: 24,
+        paddingTop: 20
+    },
+
+})
