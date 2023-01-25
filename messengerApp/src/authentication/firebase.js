1365f16d1aca155ef7d60b6e1c32cb396bf90877
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2023-01-25 20:29:30 +0100

    create firebase project

---
+++ messengerApp/src/authentication/firebase.js
@@ -0,0 +1,17 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5D8wV-q40bE2kcG7tWL09uwZ8dB8W5Vg",
+  authDomain: "loginauth-60117.firebaseapp.com",
+  projectId: "loginauth-60117",
+  storageBucket: "loginauth-60117.appspot.com",
+  messagingSenderId: "410635565601",
+  appId: "1:410635565601:web:62c51c190f2a8b2aeac160",
+  measurementId: "G-0D136729X8"
+};
+
+
+
+
+
+
