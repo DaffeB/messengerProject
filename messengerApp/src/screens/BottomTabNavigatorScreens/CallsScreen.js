0e441511dab0f93458a91e2d9cf6f80b4c51677c
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2023-02-02 20:14:34 +0100

    Working with icons in calls

--- messengerApp/src/screens/BottomTabNavigatorScreens/CallsScreen.js
+++ messengerApp/src/screens/BottomTabNavigatorScreens/CallsScreen.js
@@ -8,64 +8,232 @@ const CallScreen = () => {
         {
             id: 1,
             name: "Aley",
-            date: 'Today, 9:32 AM',
-            status: 'missed'
+            date: 'Today, 11:32 AM',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
         },
         {
             id: 2,
             name: "Aley",
             date: 'Today, 9:32 AM',
-            status: 'missed'
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
         },
         {
             id: 3,
             name: "Aley",
             date: 'Yesterday, 9:32 AM',
-            status: 'missed'
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
         },
 
         {
             id: 4,
-            name: 'Tiana',
+            name: 'Emeline',
             date: 'Jan 29',
-            status: 'incoming'
+            image: require('../../assets/images/users/woman.png'),
+            status: 'incoming',
+            icon: '',
         },
         {
             id: 5,
-            name: 'Tifanny',
+            name: 'Selma',
             date: 'Jan 22',
-            status: 'outgoing'
+            image: require('../../assets/images/users/woman1.png'),
+            status: 'outgoing',
+            icon: '',
         },
+
         {
             id: 6,
-            name: "Aley",
+            name: "Tiffany",
             date: 'Jan 12',
-            status: 'missed'
+            image: require('../../assets/images/users/woman3.png'),
+            status: 'missed',
+            icon: '',
         },
         {
             id: 7,
+            name: "Tiffany",
+            date: 'Jan 12',
+            image: require('../../assets/images/users/woman3.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 8,
+            name: "Tiffany",
+            date: 'Jan 12',
+            image: require('../../assets/images/users/woman3.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 9,
             name: "Aley",
             date: 'Jan 4',
-            status: 'missed'
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
         },
+        {
+            id: 10,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 11,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 12,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'outgoing',
+            icon: '',
+        },
+        {
+            id: 13,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'incoming',
+            icon: '',
+        },
+        {
+            id: 14,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 15,
+            name: "Joana",
+            date: 'Jan 12',
+            image: require('../../assets/images/users/woman2.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 16,
+            name: "Joana",
+            date: 'Jan 12',
+            image: require('../../assets/images/users/woman2.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 17,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 18,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 19,
+            name: "Joana",
+            date: 'Jan 12',
+            image: require('../../assets/images/users/woman2.png'),
+            status: 'missed',
+            icon: '',
+        },
+        {
+            id: 20,
+            name: 'Selma',
+            date: 'Jan 22',
+            image: require('../../assets/images/users/woman1.png'),
+            status: 'outgoing',
+            icon: '',
+        },
+
+        {
+            id: 21,
+            name: "Aley",
+            date: 'Jan 4',
+            image: require('../../assets/images/users/man.png'),
+            status: 'incoming',
+            icon: '',
+        },
+
+
+
+
+
     ]);
 
     const renderCall = ({ item }) => (
-        <ScrollView>
-            <TouchableOpacity style={styles.callContainer}>
-                <View style={styles.leftContainer}>
-                    <Text style={styles.name}>{item.name}</Text>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Text style={styles[item.status]}>{item.status}</Text>
-                        <Text style={styles.date}>{item.date}</Text>
-                        <Image />
-                    </View>
-                </View>
-                <View style={styles.rightContainer}>
 
+        <TouchableOpacity style={styles.callContainer}>
+            <Image style={{ height: 30, width: 30, marginRight: 15 }} source={item.image}></Image>
+            <View style={styles.leftContainer}>
+                <View style={styles.itemContainer}>
+                    <Image source={item.icon} style={styles.image} />
+                    <View style={styles.infoContainer}>
+                    </View>
+                    {item.status === 'missed' && (
+                        <View>
+                            <Image style={{
+                                width: 30,
+                                height: 30
+                            }} source={require('../../assets/icons/missedCall.png')} />
+                            <Text style={styles.missedCallText}>Missed</Text>
+                        </View>
+                    )}
+                    {item.status === 'incoming' && (
+                        <View style={styles.incomingCallContainer}>
+                            <Image style={{
+                                width: 30,
+                                height: 30
+                            }} source={require('../../assets/icons/videoCamera.png')} />
+                            <Text style={styles.incomingCallText}>Incoming</Text>
+                        </View>
+                    )}
+                    {item.status === 'outgoing' && (
+                        <View style={styles.outgoingCallContainer}>
+                            <Image style={{
+                                width: 30,
+                                height: 30
+                            }} source={require('../../assets/icons/missedCall.png')} />
+                            <Text style={styles.outgoingCallText}>Outgoing</Text>
+                        </View>
+                    )}
                 </View>
-            </TouchableOpacity>
-        </ScrollView>
+
+                <Text style={styles.name}>{item.name}</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles[item.status]}>{item.status}</Text>
+                    <View style={styles.circleIcon}></View>
+                    <Text style={styles.date}>{item.date}</Text>
+                    <Image />
+                </View>
+            </View>
+            <View style={styles.rightContainer}>
+
+            </View>
+        </TouchableOpacity>
+
 
     );
 
@@ -89,7 +257,8 @@ const styles = {
     },
     leftContainer: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+
     },
     rightContainer: {
         justifyContent: 'center'
@@ -108,7 +277,11 @@ const styles = {
     },
     outgoing: {
         color: 'blue'
-    }
+    },
+    circleIcon:
+        { borderWidth: 1, height: 4, width: 4, borderRadius: 4, backgroundColor: 'black' },
+
+
 };
 
 export default CallScreen;
