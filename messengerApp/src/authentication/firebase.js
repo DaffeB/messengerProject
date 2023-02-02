bfd8abc0ef5d44765d77990bac1c2b0b5468fb11
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2023-02-02 11:54:53 +0100

    change configs

--- messengerApp/src/authentication/firebase.js
+++ messengerApp/src/authentication/firebase.js
@@ -1,13 +1,14 @@
 
 
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5D8wV-q40bE2kcG7tWL09uwZ8dB8W5Vg",
-  authDomain: "loginauth-60117.firebaseapp.com",
-  projectId: "loginauth-60117",
-  storageBucket: "loginauth-60117.appspot.com",
-  messagingSenderId: "410635565601",
-  appId: "1:410635565601:web:62c51c190f2a8b2aeac160",
-  measurementId: "G-0D136729X8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 
